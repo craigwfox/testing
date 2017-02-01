cd95c443131d116c6d3bfeac4e04aef678250458
Craig Fox <craig@craigwfox.com>
2017-02-01 09:50:49 -0600

    date mask from Estelle Weyl

--- site.js
+++ site.js
@@ -1,78 +1,171 @@
+
 jQuery(document).ready(function() {
-  $('.dob-field').on('input', function() {
-    $(this).on('keydown', function(e) {
-      var v = this.value;
-      var key = event.keyCode || event.charCode;
-      if(key == 8 || key == 46) {
-      } else {
-        if (v.match(/^\d{2}$/) !== null) {
-            this.value = v + '/';
-        } else if (v.match(/^\d{2}\/\d{2}$/) !== null) {
-            this.value = v + '/';
+  
+  // $('.dob-field').on('keydown', function() {
+
+  // });
+
+  // var date = $('.dob-field').find('input');
+  // console.log(date.val());
+  // console.log(moment(date.val()).format('MM/DD/YYYY'));
+
+
+
+  var masking = {
+    // User defined Values
+    //maskedInputs : document.getElementsByClassName('masked'), // add with IE 8's death
+    maskedInputs : document.querySelectorAll('.masked'), // kill with IE 8's death
+    maskedNumber : 'XdDmMyY9',
+    maskedLetter : '_',
+
+    init: function () {
+      masking.setUpMasks(masking.maskedInputs);
+      masking.maskedInputs = document.querySelectorAll('.masked'); // Repopulating. Needed b/c static node list was created above.
+      masking.activateMasking(masking.maskedInputs);
+    },
+
+    setUpMasks: function (inputs) {
+      var i, l = inputs.length;
+
+      for(i = 0; i < l; i++) {
+        masking.createShell(inputs[i]);
+      }
+    },
+    
+    // replaces each masked input with a shall containing the input and it's mask.
+    createShell : function (input) {
+      var text = '', 
+          placeholder = input.getAttribute('placeholder');
+
+      input.setAttribute('maxlength', placeholder.length);
+      input.setAttribute('data-placeholder', placeholder);
+      input.removeAttribute('placeholder');
+
+      text = '<span class="shell">' +
+        '<span aria-hidden="true" id="' + input.id + 
+        'Mask"><i></i>' + placeholder + '</span>' + 
+        input.outerHTML +
+        '</span>';
+
+      input.outerHTML = text;
+    },
+
+    setValueOfMask : function (e) {
+      var value = e.target.value,
+          placeholder = e.target.getAttribute('data-placeholder');
+
+      return "<i>" + value + "</i>" + placeholder.substr(value.length);
+    },
+    
+    // add event listeners
+    activateMasking : function (inputs) {
+      var i, l;
+
+      for (i = 0, l = inputs.length; i < l; i++) {
+        if (masking.maskedInputs[i].addEventListener) { // remove "if" after death of IE 8
+          masking.maskedInputs[i].addEventListener('keyup', function(e) {
+            masking.handleValueChange(e);
+          }, false); 
+        } else if (masking.maskedInputs[i].attachEvent) { // For IE 8
+            masking.maskedInputs[i].attachEvent("onkeyup", function(e) {
+            e.target = e.srcElement; 
+            masking.handleValueChange(e);
+          });
         }
       }
-    });
-  });
+    },
+    
+    handleValueChange : function (e) {
+      var id = e.target.getAttribute('id');
+          
+      switch (e.keyCode) { // allows navigating thru input
+        case 20: // caplocks
+        case 17: // control
+        case 18: // option
+        case 16: // shift
+        case 37: // arrow keys
+        case 38:
+        case 39:
+        case 40:
+        case  9: // tab (let blur handle tab)
+          return;
+        }
 
+      document.getElementById(id).value = masking.handleCurrentValue(e);
+      document.getElementById(id + 'Mask').innerHTML = masking.setValueOfMask(e);
 
+    },
 
-  var element = $('input');
-  //keyboard maping array   
-  var keyboardMap = ["", "", "", "CANCEL", "", "", "HELP", "", "BACK_SPACE", "TAB", "", "", "CLEAR", "ENTER", "RETURN", "", "SHIFT", "CONTROL", "ALT", "PAUSE", "CAPS_LOCK", "KANA", "EISU", "JUNJA", "FINAL", "HANJA", "", "ESCAPE", "CONVERT", "NONCONVERT", "ACCEPT", "MODECHANGE", "SPACE", "PAGE_UP", "PAGE_DOWN", "END", "HOME", "LEFT", "UP", "RIGHT", "DOWN", "SELECT", "PRINT", "EXECUTE", "PRINTSCREEN", "INSERT", "DELETE", "", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "COLON", "SEMICOLON", "LESS_THAN", "EQUALS", "GREATER_THAN", "QUESTION_MARK", "AT", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "WIN", "", "CONTEXT_MENU", "", "SLEEP", "NUMPAD0", "NUMPAD1", "NUMPAD2", "NUMPAD3", "NUMPAD4", "NUMPAD5", "NUMPAD6", "NUMPAD7", "NUMPAD8", "NUMPAD9", "MULTIPLY", "ADD", "SEPARATOR", "SUBTRACT", "DECIMAL", "DIVIDE", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13", "F14", "F15", "F16", "F17", "F18", "F19", "F20", "F21", "F22", "F23", "F24", "", "", "", "", "", "", "", "", "NUM_LOCK", "SCROLL_LOCK", "WIN_OEM_FJ_JISHO", "WIN_OEM_FJ_MASSHOU", "WIN_OEM_FJ_TOUROKU", "WIN_OEM_FJ_LOYA", "WIN_OEM_FJ_ROYA", "", "", "", "", "", "", "", "", "", "CIRCUMFLEX", "EXCLAMATION", "DOUBLE_QUOTE", "HASH", "DOLLAR", "PERCENT", "AMPERSAND", "UNDERSCORE", "OPEN_PAREN", "CLOSE_PAREN", "ASTERISK", "PLUS", "PIPE", "HYPHEN_MINUS", "OPEN_CURLY_BRACKET", "CLOSE_CURLY_BRACKET", "TILDE", "", "", "", "", "VOLUME_MUTE", "VOLUME_DOWN", "VOLUME_UP", "", "", "", "", "COMMA", "", "PERIOD", "SLASH", "BACK_QUOTE", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "OPEN_BRACKET", "BACK_SLASH", "CLOSE_BRACKET", "QUOTE", "", "META", "ALTGR", "", "WIN_ICO_HELP", "WIN_ICO_00", "", "WIN_ICO_CLEAR", "", "", "WIN_OEM_RESET", "WIN_OEM_JUMP", "WIN_OEM_PA1", "WIN_OEM_PA2", "WIN_OEM_PA3", "WIN_OEM_WSCTRL", "WIN_OEM_CUSEL", "WIN_OEM_ATTN", "WIN_OEM_FINISH", "WIN_OEM_COPY", "WIN_OEM_AUTO", "WIN_OEM_ENLW", "WIN_OEM_BACKTAB", "ATTN", "CRSEL", "EXSEL", "EREOF", "PLAY", "ZOOM", "", "PA1", "WIN_OEM_CLEAR", ""];
+    handleCurrentValue : function (e) {
+      var isCharsetPresent = e.target.getAttribute('data-charset'), 
+          placeholder = isCharsetPresent || e.target.getAttribute('data-placeholder'),
+          value = e.target.value, l = placeholder.length, newValue = '', 
+          i, j, isInt, isLetter, strippedValue;
 
-  //Allowed keys as per your requirement          
-  var allowedKey = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "NUMPAD0", "NUMPAD1", "NUMPAD2", "NUMPAD3", "NUMPAD4", "NUMPAD5", "NUMPAD6", "NUMPAD7", "NUMPAD8", "NUMPAD9", "DELETE", "BACK_SPACE"];
+      // strip special characters
+      strippedValue = isCharsetPresent ? value.replace(/\W/g, "") : value.replace(/\D/g, "");
 
-  var keyDel = ["DELETE", "BACK_SPACE"];
+      for (i = 0, j = 0; i < l; i++) {
+          var x = 
+          isInt = !isNaN(parseInt(strippedValue[j]));
+          isLetter = strippedValue[j] ? strippedValue[j].match(/[A-Z]/i) : false;
+          matchesNumber = masking.maskedNumber.indexOf(placeholder[i]) >= 0;
+          matchesLetter = masking.maskedLetter.indexOf(placeholder[i]) >= 0;
 
-  //bind keydown to your element
-  element.on("keydown", function (e) {
-    var key = e.charCode || e.keyCode || 0;
-    var keyValue = keyboardMap[key];
-    $('#log').html(keyValue);
-    // if ($.inArray(keyValue, keyDel) !== -1) {
-    //   console.log('deleteing');
-    //   $('#log').html('deleteing');
-    //   return true;
-    // } else if ($.inArray(keyValue, allowedKey) !== -1){
-    //   console.log('allowed');
-    //   $('#log').html('allowed');
-    //   return true;
-    // }
-    // else{
-    //   console.log('not allowed');
-    //   $('#log').html('not allowed');
-    //   return  false;
-    // }
-  })
+          if ((matchesNumber && isInt) || (isCharsetPresent && matchesLetter && isLetter)) {
+
+                  newValue += strippedValue[j++];
+
+            } else if ((!isCharsetPresent && !isInt && matchesNumber) || (isCharsetPresent && ((matchesLetter && !isLetter) || (matchesNumber && !isInt)))) {
+                  // masking.errorOnKeyEntry(); // write your own error handling function
+                  return newValue; 
+
+          } else {
+              newValue += placeholder[i];
+          } 
+          // break if no characters left and the pattern is non-special character
+          if (strippedValue[j] == undefined) { 
+            break;
+          }
+      }
+      if (e.target.getAttribute('data-valid-example')) {
+        return masking.validateProgress(e, newValue);
+      }
+      return newValue;
+    },
+
+    validateProgress : function (e, value) {
+      var validExample = e.target.getAttribute('data-valid-example'),
+          pattern = new RegExp(e.target.getAttribute('pattern')),
+          placeholder = e.target.getAttribute('data-placeholder'),
+          l = value.length, testValue = '';
+
+      //convert to months
+      if (l == 1 && placeholder.toUpperCase().substr(0,2) == 'MM') {
+        if(value > 1 && value < 10) {
+          value = '0' + value;
+        }
+        return value;
+      }
+      // test the value, removing the last character, until what you have is a submatch
+      for ( i = l; i >= 0; i--) {
+        testValue = value + validExample.substr(value.length);
+        if (pattern.test(testValue)) {
+          return value;
+        } else {
+          value = value.substr(0, value.length-1);
+        }
+      }
+    
+      return value;
+    },
+
+    errorOnKeyEntry : function () {
+      // Write your own error handling
+    }
+  }
+
+  masking.init();
+
 });
-
-  // var detectDelete = function(event) {
-  //   var key = event.keyCode || event.charCode;
-  //   if(key == 8 || key == 46) {
-  //     alert('delete pressed');
-  //   }
-  // };
-  // detectDelete();
-
-  // $(".inputTxt").bind("keypress keyup keydown", function (event) {
-  //   var evtType = event.type;
-  //   var eWhich = event.which;
-  //   var echarCode = event.charCode;
-  //   var ekeyCode = event.keyCode;
-
-  //   switch (evtType) {
-  //       // case 'keypress':
-  //       //     $("#log").html("<b>" + evtType + "</b>" + " keycode: " + ekeyCode + " charcode: " + echarCode + " which: " + eWhich + "<br>");
-  //       //     break;
-  //       case 'keyup':
-  //           $("#log").html("<b>" + evtType + "</b>" + " keycode: " + ekeyCode + " charcode: " + echarCode + " which: " + eWhich + "<p>");
-  //           break;
-  //       // case 'keydown':
-  //       //     $("#log").html("<b>" + evtType + "</b>" + " keycode: " + ekeyCode + " charcode: " + echarCode + " which: " + eWhich + "<br>");
-  //       //     break;
-  //       default:
-  //           break;
-  //   }
-  // });
 
